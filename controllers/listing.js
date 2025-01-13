c8e8e6747a442561c3acb61450e043effc737eda
Uday Goel <udaygoel295@gmail.com>
2025-01-13 19:11:28 +0530

    map integration

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,7 +1,7 @@
 const Listing = require("../models/listing.js");
-const maptilerClient = require("@maptiler/client");
-const mapAPI = process.env.MAP_API;
-maptilerClient.config.apiKey = mapAPI;
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -23,17 +23,19 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res) => {
-  const result = await maptilerClient.geocoding.forward(
-    req.body.listing.location,
-    { limit: 1 }
-  );
+  const response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
   let url = req.file.path;
   let filename = req.file.filename;
-
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
-  newListing.geometry = result.features[0].geometry;
+  newListing.geometry = response.features[0].geometry;
   await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,32 @@
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+  "use strict";
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+// Mapbox
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9, // starting zoom
+});
+
+const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
