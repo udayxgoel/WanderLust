b22cfbe6d1cf68f20982a35fd4e9bc7028df6035
Uday Goel <udaygoel295@gmail.com>
2025-01-13 19:19:45 +0530

    map fixes

--- public/js/script.js
+++ public/js/script.js
@@ -26,7 +26,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9, // starting zoom
+  zoom: 12, // starting zoom
 });
 
 const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
