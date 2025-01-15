254cadddc99961656403dce3fda3c494e3d7809f
Uday Goel <udaygoel295@gmail.com>
2025-01-15 19:25:13 +0530

    map fixes

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const Listing = require("../models/listing.js");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapToken = process.env.MAP_TOKEN;
